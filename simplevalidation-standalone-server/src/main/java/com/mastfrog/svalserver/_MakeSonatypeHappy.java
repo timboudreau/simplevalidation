f5bcf1c2f8df81b8c622eef2871876bc761cf24e
Tim Boudreau <tim@timboudreau.com>
2019-12-03 14:29:37 -0500

    Fix pom version

---
+++ simplevalidation-standalone-server/src/main/java/com/mastfrog/svalserver/_MakeSonatypeHappy.java
@@ -0,0 +1,15 @@
+package com.mastfrog.svalserver;
+
+/**
+ * This class exists to generate empty javadoc and source jars to
+ * satisfy Sonatype's staging repository.  This project is simply
+ * an aggregation of its dependencies with some omitted.
+ *
+ * @author Tim Boudreau
+ */
+public final class _MakeSonatypeHappy {
+
+    private _MakeSonatypeHappy() {
+        throw new AssertionError();
+    }
+}
---
+++ standalone/src/main/java/com/mastfrog/svstandalone/_MakeSonatypeHappy.java
@@ -0,0 +1,15 @@
+package com.mastfrog.svstandalone;
+
+/**
+ * This class exists to generate empty javadoc and source jars to
+ * satisfy Sonatype's staging repository.  This project is simply
+ * an amalgamation of 
+ *
+ * @author Tim Boudreau
+ */
+public final class _MakeSonatypeHappy {
+
+    private _MakeSonatypeHappy() {
+        throw new AssertionError();
+    }
+}
